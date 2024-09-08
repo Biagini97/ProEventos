3aa2dd79b99c5b0472d88045c2ee557ebe9f645a
Luiz Fernando Biagini <luizbiagini97@gmail.com>
2024-09-08 17:02:44 -0300

    Filtro Ajustes de Layout

--- ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   widthImag = 80;
   margImg = 2;
+  isCollapsed = false;
+  // tslint:disable-next-line:variable-name
+  private _filtroLista = '';
 
+  // tslint:disable-next-line:typedef
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+
+  }
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -20,10 +42,16 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void{
     this.http.get( 'https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+
       error => console.log(error)
     );
   }
 
 
 }
+
+
